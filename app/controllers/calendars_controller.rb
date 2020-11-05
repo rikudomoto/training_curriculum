84022bc6132e97e26fb47c9fc0f81a90ec139615
rikudomoto <73264978+rikudomoto@users.noreply.github.com>
2020-11-05 18:56:47 +0900

    めっちゃ修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -22,11 +22,6 @@ class CalendarsController < ApplicationController
 
   def get_Week
     days = Date
-<<<<<<< Updated upstream
-=======
-
-  def get_Week
->>>>>>> Stashed changes
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
     wdays[Date.today.wday]
 
@@ -44,18 +39,9 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-<<<<<<< Updated upstream
-      days = { month: '(@todays_date + x).month', date: '(@todays_date+x).day', plans: 'today_plans' ,wdays: 'wday'}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans ,wdays: wday}
       @week_days.push(days)
       end
 
      end
    end
-=======
-      days = { month: '(@todays_date + x).month', date: '(@todays_date+x).day', plans: 'today_plans', wdays: 'wday' }
-      @week_days.push(days)
-    end
-  end
-end
-end
->>>>>>> Stashed changes
